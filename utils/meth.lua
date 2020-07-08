ae431cee7cf8b642a062ce027e964d4f6f14b5d6
Adam Hellberg <sharparam@sharparam.com>
2020-07-08 05:24:27 +0200

    Add meth helpers for Border Crystals

--- utils/meth.lua
+++ utils/meth.lua
@@ -9,15 +9,18 @@ local ingredient_hints = {
     pln_rat_stage1_20 = 'Add Muriatic Acid',
     pln_rat_stage1_22 = 'Add Caustic Soda',
     pln_rat_stage1_24 = 'Add Hydrogen Chloride',
-    --pln_rat_stage1_28 = 'Meth batch is complete'
+    --pln_rat_stage1_28 = 'Meth batch is complete',
+    Play_loc_mex_cook_03 = 'Add Muriatic Acid',
+    Play_loc_mex_cook_04 = 'Add Caustic Soda',
+    Play_loc_mex_cook_05 = 'Add Hydrogen Chloride'
 }
 
 local _queue_dialog_orig = DialogManager.queue_dialog
 function DialogManager:queue_dialog(id, ...)
     log:verbose('Dialog ID: %s', id)
 
-    if ingredient_hints[id] then
-        local text = ingredient_hints[id]
+    local text = ingredient_hints[id]
+    if text then
         log:info(text):system(text):hint(text):objective('Objective updated', text)
     end
 
